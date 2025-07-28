a06d6099ad9144043c9b1d9afa50edff2e42453a
pascal-maker <pascal-musa@hotmail.com>
2025-07-28 14:39:33 +0200

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,19 +1,21 @@
-select orderid,employeeid 
-from tblorders;
 
-# ex 2:
-select productnumber, productname, priceperunit,
-concat(stock,' pieces') as 'Stock',
-concat(priceperunit * stock) as 'Stock value'
+SELECT ProductNumber,ProductName,PricePerUnit,Stock,PricePerUnit, concat(Stock ,'pieces'),concat (PricePerUnit*Stock,'€') AS 'Stockvalue' from tblproducts;
+#Exercise2
+SELECT ProductNumber,ProductName,PricePerUnit AS 'Purchaseprice', concat (PricePerUnit * 1.15) AS 'SellingPrice', concat((PricePerUnit * 1.15) - PricePerUnit,'€') AS 'Profit'
 from tblproducts;
-
-
-
-
-# ex 3
-select customerid, name, type
-from tblcustomers
-where city = 'Tienen';
-
-
-
+#Exercise3
+select CustomerId,Name,Type from tblcustomers WHERE City = "Tienen";
+#Exercise4
+select ProductName from tblproducts WHERE CategoryNumber = 1;
+#Exercise5
+select Name from tblcustomers where City = "Genk" and Zipcode = 3600;
+#Exercise6
+select Name from tblcustomers where City = "Leuven" and Zipcode = 3000;
+#Exercise7
+select Name,City,Type from tblcustomers where Type = 'P' And Balance > 0  And City = "Tienen";
+#Exercise8
+select Name,City from tblcustomers where City = "Leuven" OR City = "Herent";
+#Exercise9
+select Name,Address,Type,City,Zipcode, concat (Zipcode, '' ,City) AS City from  tblcustomers where Type = 'T' OR Type = 'W';
+#Exercise10
+select EmployeeID, LastName,FirstName,BirthDate , concat (LastName, '',FirstName) AS Fullname from tblemployees where BirthDate  <= '1950-01-01';
